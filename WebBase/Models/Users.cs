52deacb23acfa2502ddbf2cea5f68b2f441f46c2
SEO <SEO@SEO>
2020-04-10 20:27:09 +0430

    Add project files.

---
+++ WebBase/Models/Users.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace WebBase.Models
+{
+    public class Users
+    {
+        public int ID { get; set; }
+        public string FName { get; set; }
+        public string Lname { get; set; }
+        public string FullName { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public DateTime RegisterDate { get; set; }
+
+    }
+}
